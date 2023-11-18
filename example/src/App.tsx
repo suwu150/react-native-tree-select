3a2e6e8135e201f3c589905bd2ea0a9f58a615c2
jikang.wu <jikang.wu@kikitrade.com>
2023-11-18 22:00:23 +0800

    feat: update dev

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
